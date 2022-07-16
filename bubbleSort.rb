4b91d114a8d5423cbf29ae773fd08c0bc356ed15
Daniel Freire <danielfreire.web@gmail.com>
2022-07-16 20:35:33 +0100

    sorting works 1st time

--- bubbleSort.rb
+++ bubbleSort.rb
@@ -1,31 +1,49 @@
 def bubble_sort(array)
+     return array if array.length <= 1
     #create arrays
     array = array
     pre_array = []
     mid_array = []
     post_array = []
     i=0
+    j=0
 
     # array shall be split into 3 parts: pair that will be reverted;  pre-pair; post-pair
 
-    
-    while array[i] > array[i+1]
-        if array[i] > array[i+1]
-            if i >= 1
-                pre_array = array.shift i-1
+    for i in 0..array.length-1 do
+        for j in 0..array.length-1 do
+            if array[j] > array[j+1]
+                if j < 1
+                    mid_array = array.shift 2
+                    #puts "j<1 mid_array is #{mid_array}"
+                    mid_array = mid_array.reverse
+                    #puts "j<1 reversed mid_array is #{mid_array}"
+                    post_array = array
+                    array = pre_array + mid_array + post_array
+                   
+                elsif j==array.length-1
+                    array
+                else
+                    pre_array = array.shift j
+                    #puts "pre_array is #{pre_array}"
+                    mid_array = array.shift 2
+                    mid_array = mid_array.reverse
+                    #puts "reversed mid_array is #{mid_array}"
+                    post_array = array
+                    array = pre_array + mid_array + post_array    
+                end
+                
+                puts "(if) Sorted array is '#{array}'"
+                j+=1
             end
-            mid_array = array.shift 2
-            mid_array = mid_array.reverse
-            post_array = array
-            array = pre_array + mid_array + post_array
-            puts "(if) Sorted array is '#{array}'"
-            #i = i + 1
+            puts "(for j) Sorted array is '#{array}'"
+            
         end
-        i = i + 1 
-        puts "(while) Sorted array is '#{array}'"
-    end
-    puts "Sorted array is '#{array}'"    
+        
+        puts "(for i) array is '#{array}'"  
+    end  
 end
 
 
-bubble_sort([4,3,78,2,0,2])
+#bubble_sort([4,3,78,2,0,2])
+bubble_sort([5,4,3,2,1,0])
---
+++ bubbleSort2.rb
@@ -0,0 +1,7 @@
+def bubble_sort(array)
+    #solution could be each_cons()
+end
+
+
+bubble_sort([4,3,78,2,0,2])
+#bubble_sort([5,4,3,2,1,0])
